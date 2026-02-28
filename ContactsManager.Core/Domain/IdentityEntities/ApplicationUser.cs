4a3b3901872ea0f2cbf715d53986d2e989b03d0a
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2026-02-28 10:43:49 +0200

    Identity, Authorization, Security [MVC and Web API] Creating Models

---
+++ ContactsManager.Core/Domain/IdentityEntities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace ContactsManager.Core.Domain.IdentityEntities;
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string? PersonNAme { get; set; }
+}
